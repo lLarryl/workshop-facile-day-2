cdce87d2c220e877c03cbe4b97eb521f474052d6
Vittorio Larovere <vlarovere@CLIFMI522.fritz.box>
2020-11-30 10:00:54 +0100

    test object

--- project/src/index.ts
+++ project/src/index.ts
@@ -1,2 +1,19 @@
-// istanbul ignore file
-console.log("ok");
+const a: object = {
+    a: "",
+    b: 1
+}
+
+const b: object = {
+    c: ""
+}
+
+const c: object = {...a, ...b};
+
+const r: Readonly<Record<"a" | "b", number>> = {
+    a: 0,
+    b: 1
+}
+
+type numberReadonlyArray = Readonly<Array<number>>
+
+r["a"] = 2
