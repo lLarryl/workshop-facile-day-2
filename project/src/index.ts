c95ca6a765ea11160d52639b8cad1f6223047948
Vittorio Larovere <vlarovere@CLIFMI522.fritz.box>
2020-11-30 10:15:33 +0100

    esercizio 3 Map

--- project/src/index.ts
+++ project/src/index.ts
@@ -17,3 +17,12 @@ export const myRecord: Record<string, string> = {
     a: 1,
     b: "b",
   };
+
+  type numberReadonlyArray = Array<number>
+
+  const myMap = new Map<{a: string, b: string}, string>();
+
+  const x = {a: "", b: ""};
+
+  myMap.set(x, "");
+  myMap.get(x);
